e30168cfbef8afd43abafdaf81a48b607a302cf5
Dmide <revdmide@gmail.com>
2018-11-22 16:30:27 +0300

    RxJavaPlugins errorHandler

--- app/src/main/java/com/dmide/revolutassignment/app/CurrenciesApplication.kt
+++ app/src/main/java/com/dmide/revolutassignment/app/CurrenciesApplication.kt
@@ -1,19 +1,26 @@
 package com.dmide.revolutassignment.app
 
 import android.app.Application
+import android.util.Log
 import com.dmide.revolutassignment.inject.component.AppComponent
 import com.dmide.revolutassignment.inject.component.DaggerAppComponent
 import com.dmide.revolutassignment.inject.module.AppModule
 import com.dmide.revolutassignment.inject.module.NetworkModule
+import io.reactivex.plugins.RxJavaPlugins
 
 class CurrenciesApplication : Application() {
 
     val component: AppComponent by lazy {
         DaggerAppComponent
-                .builder()
-                .appModule(AppModule(this))
-                .networkModule(NetworkModule)
-                .build()
+            .builder()
+            .appModule(AppModule(this))
+            .networkModule(NetworkModule)
+            .build()
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        // see https://github.com/ReactiveX/RxJava/wiki/What's-different-in-2.0#error-handling
+        RxJavaPlugins.setErrorHandler { t -> Log.e(javaClass.name, "", t) }
+    }
 }
